f9195e8c80ede30259d9e93f14860e742c96a8f1
Nabeyou Tadessa <nabeyoutadessa@gmail.com>
2024-01-10 11:06:59 -0500

    Changed starting location and Nav size

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,7 +4,7 @@ import 'leaflet/dist/leaflet.css';
 
 const MapComponent = () => {
   return (
-    <MapContainer center={[34.6834, -82.8374]} zoom={13} style={{ height: '100vh', width: '100%' }}>
+    <MapContainer center={[34.6834, -82.8374]} zoom={12} style={{ height: '100vh', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
